8ca5805c702466d816611021792902c421d12b38
sudheshtnamupparapu1999@gmail.com <sudheshtnamupparapu1999@gmail.com>
2022-09-02 19:41:28 +0530

    Added: arrayrepeated

--- arrayrepeated.sh
+++ arrayrepeated.sh
@@ -1,12 +1,18 @@
-#printing array elements
 echo -n "Number : "
 read num;
 
+array=()
 for i in $(seq 1 $num)
 do
    if [ $(expr $i % 11) == 0 ]
    then
-        echo $i
+        array+=($i)
    fi
 done
+
+for value in "${array[@]}"
+do
+     echo $value
+done
+
 
